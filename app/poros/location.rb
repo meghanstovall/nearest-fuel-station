a73863a62ec6678cbd181f34df0c5b66e093f1dd
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-10 10:33:49 -0600

    Create location poro and service for nrel api

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -2,10 +2,21 @@ class Location
 
   def initialize(address)
     @address = address.split(" ")
-    require "pry"; binding.pry
   end
 
   def street
-    @address[0..2]
+    @address[0..2].join(" ")
+  end
+
+  def city
+    @address[-3][0...-1]
+  end
+
+  def state
+    @address[-2]
+  end
+
+  def zip
+    @address[-1]
   end
 end
---
+++ app/services/nrel_service.rb
@@ -0,0 +1,12 @@
+class NRELService
+
+  def self.conn(location)
+    stations_response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location.street},#{location.city},#{location.state},#{location.zip}&api_key=#{ENV['NREL_API_KEY']}")
+    get_data(JSON.parse(stations_response.body, symbolize_names: true))
+  end
+
+  private
+    def self.get_data(json)
+      station = json[:fuel_stations].first
+    end
+end
